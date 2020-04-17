bbb75a5ec06f9d69121c117b912af44928c40dd1
Maximous Black <maximousblk@gmail.com>
2020-04-18 01:27:31 +0530

    added configurations

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Piracy',
+    description: 'A curated list of aarrrr!',
+        themeConfig: {
+        nav: [{ text: 'Home', link: '/' }]
+    },
+    dest: 'dist'
+  }
