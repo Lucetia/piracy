68b8adff71b2505fb400c2767355f6e85906a846
Maximous Black <maximousblk@gmail.com>
2020-04-30 01:03:13 +0530

    Added meta tags

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,45 @@ module.exports = {
     title: "Piracy",
     description: "It's illegal cuz they can't tax you!",
     dest: "dist",
-    head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+    head: [
+        ["link", { rel: "icon", href: "/favicon.ico" }],
+        ["meta", { name: "theme-color", content: "#3eaf7c" }],
+        ["meta", { property: "theme-color", content: "#3eaf7c" }],
+
+        ["meta", { property: "og:type", content: "website" }],
+        ["meta", { property: "og:url", content: "https://piracy.now.sh/" }],
+        ["meta", { property: "og:title", content: "Piracy" }],
+        [
+            "meta",
+            {
+                property: "og:description",
+                content: "It's illegal cuz they can't tax you!",
+            },
+        ],
+        ["meta", { property: "og:image", content: "/image.png" }],
+
+        ["meta", { property: "twitter:card", content: "summary_large_image" }],
+        [
+            "meta",
+            { property: "twitter:url", content: "https://piracy.now.sh/" },
+        ],
+        ["meta", { property: "twitter:title", content: "Piracy" }],
+        [
+            "meta",
+            {
+                property: "twitter:description",
+                content: "It's illegal cuz they can't tax you!",
+            },
+        ],
+        ["meta", { property: "twitter:image", content: "/image.png" }],
+        [
+            "meta",
+            {
+                name: "google-site-verification",
+                content: "M4wcsX_DZ9CkpAzZ5rNmUbk1JWl3aLqgxIfB4YG-ozI",
+            },
+        ],
+    ],
     themeConfig: {
         logo: "/logo.svg",
         nav: [
